4f8344c489b1da2bec3d5af573b85a82d91977f2
Tahir Ahmed <01tahirahmed@gmail.com>
2023-04-07 17:19:16 +0530

    how to work with arrays some advance array methods

--- arrays/array_method.php
+++ arrays/array_method.php
@@ -46,6 +46,57 @@
  $keys = array_keys($array1);
  printArray($keys);
 
+ // array_marge($array ...$array):array
+ $array2 = [1, 2, 3];
+ $array3 = ['a' => 4, 7 => 5, 8 => 6];
+ $array4 = [7, 8, 9];
+
+ $merged = array_merge($array2, $array3, $array4);
+
+ printArray($merged);
+
+ // array_reduce() 
+ $invoiceItems = [
+   ['price' => 9.99, 'qty' => 3, 'desc' => 'item 1'],
+   ['price' => 29.99, 'qty' => 2, 'desc' => 'item 2'],
+   ['price' => 149, 'qty' => 1, 'desc' => 'item 3'],
+   ['price' => 39, 'qty' => 1, 'desc' => 'item 4'],
+   ['price' => 12.5, 'qty' => 6, 'desc' => 'item 5']
+ ];
+
+ $total = array_reduce($invoiceItems, function($sum, $item) {
+    return $sum + $item['price'] * $item['qty'];
+ });
+
+echo $total;
+echo "<br>";
+// you can also use this 👇🏽
+// $total;
+//    foreach($invoiceItems as $item) {
+//      global $total;
+//      $total += $item['price'] * $item['qty'];
+     
+//    }
+
+// array_search
+$array5 = ['a', 'b', 'c', 'D', 'E', 'ab', 'bc'];
+$key1 = array_search('e', $array5);
+
+// if($key1 === false) {
+//     echo "Letter not found!<br>";
+// } else {
+//     var_dump($key1);
+// }
+
+if(in_array('tm', $array5)) {
+    echo "Letter found!<br>";
+} else {
+    echo "Letter not found!<br>";
+}
+
+
+
+   
 
 
  
